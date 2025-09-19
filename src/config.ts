5173a9f96c5a3adf135573c522c58e31bd408b9f
Arnold Bonit <arnoldbonit@gmail.com>
2025-09-19 16:16:24 +0000

    Ye dusre batch ka drive url hai isko convert kr ke add kro :- https://dr

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,15 @@
 export const BATCHES = [
   {
-    id: "bpsc70",
-    jsonUrl: "https://drive.google.com/uc?id=YOUR_FILE_ID"
+    id: 'bpsc70',
+    jsonUrl:
+      'https://drive.google.com/uc?id=14797mbXg9dXYFOKKbcWb6GHcLV8JustP',
   },
   {
-    id: "ethics",
-    jsonUrl: "https://drive.google.com/uc?id=YOUR_FILE_ID"
+    id: 'ethics',
+    jsonUrl: 'https://drive.google.com/uc?id=YOUR_FILE_ID',
   },
   {
-    id: "upsc-287",
-    jsonUrl: "" // Will use local file
-  }
+    id: 'upsc-287',
+    jsonUrl: '', // Will use local file
+  },
 ];
