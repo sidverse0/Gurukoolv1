2d4ae6c11e208a23676081fb05e4f53886565450
Arnold Bonit <arnoldbonit@gmail.com>
2025-09-19 20:39:42 +0000

    Profile page ko acche se ui/ux design ka bnao. Aur app name header se ma

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -44,40 +44,7 @@ export function Header() {
 
       <div className="flex-1" />
 
-      {user && (
-         <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-                <Avatar className="h-10 w-10">
-                   <AvatarImage
-                    src={`https://api.dicebear.com/8.x/bottts/svg?seed=${user.uid}`}
-                  />
-                  <AvatarFallback>{getInitials(user.email)}</AvatarFallback>
-                </Avatar>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-              <DropdownMenuLabel className="font-normal">
-                <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">{user.displayName || user.email?.split('@')[0]}</p>
-                  <p className="text-xs leading-none text-muted-foreground">
-                    {user.email}
-                  </p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => router.push('/profile')}>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={handleLogout}>
-                <LogOut className="mr-2 h-4 w-4" />
-                <span>Log out</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-      )}
+      {/* Profile Dropdown Removed as requested */}
     </header>
   );
 }
