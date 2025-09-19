d922fd65b7f58e52f0eca9e9f44e58a9a346896c
Arnold Bonit <arnoldbonit@gmail.com>
2025-09-19 18:45:57 +0000

    Ye jo top left corner me icon hai sidebar usko remove kro uske logic and

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,25 +1,15 @@
 
-import {
-  Sidebar,
-  SidebarProvider,
-} from '@/components/ui/sidebar';
-import { AppSidebarContent } from './app-sidebar-content';
 import { BottomNav } from './bottom-nav';
 import { Header } from './header';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <Sidebar>
-        <AppSidebarContent />
-      </Sidebar>
-      <div className="flex-1">
-        <Header />
-        <main className="min-h-screen p-4 pb-20 md:p-6 md:pb-6">
-          {children}
-        </main>
-      </div>
+    <div className="flex-1">
+      <Header />
+      <main className="min-h-screen p-4 pb-20 md:p-6 md:pb-6">
+        {children}
+      </main>
       <BottomNav />
-    </SidebarProvider>
+    </div>
   );
 }
