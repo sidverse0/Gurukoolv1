2aff1c92bd9118f66c016c23f4f94708ce843a25
Arnold Bonit <arnoldbonit@gmail.com>
2025-09-20 05:06:11 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/contexts/theme-provider.tsx
+++ src/contexts/theme-provider.tsx
@@ -1,19 +1,12 @@
+
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
-export function useTheme() {
-    const context = React.useContext(require('next-themes').ThemeContext)
-    if (context === undefined) {
-        throw new Error("useTheme must be used within a ThemeProvider")
-    }
-    return context
-}
-
-    
+export { useTheme }
